5ccfcc010063a8f18ae67c3b8a32195bf2b18bd8
ThePotatoGuy <andreponce@gmx.com>
2014-04-22 20:46:56 -0700

    commented out debug lines

--- PoliticalUnit.java
+++ PoliticalUnit.java
@@ -5,7 +5,7 @@ public class PoliticalUnit{
     Years span;// = new Years();
     ArrayList<Notes> events, accomplishments, people;
     public PoliticalUnit(String n, Years y){
-    	System.out.println("creating");
+    	//System.out.println("creating");
         name = n;
         span = y;
         events = new ArrayList<Notes>();
